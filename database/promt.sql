c8789aca8d04a3cfd635b33b83d4e899bcbdd857
Lionixz <rodeldeleon199718@gmail.com>
2025-07-19 01:25:32 +0800

    word intensity list15 Done

--- database/promt.sql
+++ database/promt.sql
@@ -93,10 +93,6 @@ CREATE TABLE `general` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-
-
-
-
 -- delete data
 DROP TABLE IF EXISTS `verbal`;
 DROP TABLE IF EXISTS `numerical`;
